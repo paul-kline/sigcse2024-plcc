2a9ef1c77ddd2451061a8abafad520f395b10880
Stoney Jackson <dr.stoney@gmail.com>
2023-10-12 21:44:17 -0400

    add LICENSE and GitPod

---
+++ .gitpod/install-plcc.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# SPDX-FileCopyrightText: 2023 Stoney Jackson <dr.stoney@gmail.com>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+rm -rf "$HOME/plcc"
+git clone https://github.com/ourPLCC/plcc.git "$HOME/plcc"
+git -C "$HOME/plcc" checkout v3.3.1
+
+# Define LIBPLCC and add it to PATH in ~/.bashrc
+echo 'export LIBPLCC="$HOME/plcc/src"
+export PATH="$LIBPLCC:$PATH"
+' >> "$HOME/.bashrc"
+
+echo "\n    Open a new terminal.\n"
